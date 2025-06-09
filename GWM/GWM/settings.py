18555f649fc234a96943f94104876bee2b9f65ed
Pedro Benites <pedrobpfeitosa@gmail.com>
2025-06-09 17:46:46 -0300

    configuração do deploy render

--- GWM/GWM/settings.py
+++ GWM/GWM/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z2c@*9#68g19z4jk)j)i)or92w6n@ql4*^+3cp#x3_=%yn_e1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gwm-so5v.onrender.com']
 
 
 # Application definition
