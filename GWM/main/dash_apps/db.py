5e8de88092bdd4b4d899237d8f89c7a49f4aa665
Ejempedrobenites <pedro.benites@ejemmackenzie.com.br>
2025-08-21 15:21:29 -0300

    Comentando db.py

--- GWM/main/dash_apps/db.py
+++ GWM/main/dash_apps/db.py
@@ -1,46 +1,46 @@
-import psycopg2
-import pandas as pd
+# import psycopg2
+# import pandas as pd
+
+# # def conectar():
+# #     return psycopg2.connect(
+# #     dbname="gwm1807",
+# #     user="gwm1807_user",
+# #     password="Sx9MIqQ160xrb8kaFN0Mh3Jezc9jc1NY",
+# #     host="dpg-d1sndaali9vc73c96o3g-a.virginia-postgres.render.com",
+# #     port="5432",
+# #     sslmode='require'
+# #     )
 
 # def conectar():
 #     return psycopg2.connect(
-#     dbname="gwm1807",
-#     user="gwm1807_user",
-#     password="Sx9MIqQ160xrb8kaFN0Mh3Jezc9jc1NY",
-#     host="dpg-d1sndaali9vc73c96o3g-a.virginia-postgres.render.com",
+#     dbname="gwm",
+#     user="adm",
+#     password="admingwm",
+#     host="localhost",
 #     port="5432",
-#     sslmode='require'
 #     )
 
-def conectar():
-    return psycopg2.connect(
-    dbname="gwm",
-    user="adm",
-    password="admingwm",
-    host="localhost",
-    port="5432",
-    )
-
-def pegar_planejado():
-    conn = conectar()
-    query = "SELECT * FROM main_planejado;"  # Altere para sua view ou tabela correta
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
+# def pegar_planejado():
+#     conn = conectar()
+#     query = "SELECT * FROM main_planejado;"  # Altere para sua view ou tabela correta
+#     df = pd.read_sql(query, conn)
+#     conn.close()
+#     return df
 
 
-def pegar_realizado():
-    conn = conectar()
-    query = "SELECT * FROM main_executado;"  # Altere para sua view ou tabela correta
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
+# def pegar_realizado():
+#     conn = conectar()
+#     query = "SELECT * FROM main_executado;"  # Altere para sua view ou tabela correta
+#     df = pd.read_sql(query, conn)
+#     conn.close()
+#     return df
 
 
-def pegar_captacao():
-    conn = conectar()
-    query = "SELECT * FROM main_captacao;"  # Altere para sua view ou tabela correta
-    df = pd.read_sql(query, conn)
-    conn.close()
-    return df
+# def pegar_captacao():
+#     conn = conectar()
+#     query = "SELECT * FROM main_captacao;"  # Altere para sua view ou tabela correta
+#     df = pd.read_sql(query, conn)
+#     conn.close()
+#     return df
     
    
